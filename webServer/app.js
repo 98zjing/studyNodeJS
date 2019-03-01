dd23924ec31447a1528c4a20e969b34f9ca029fc
zns <1534741136@qq.com>
2019-03-01 14:00:13 +0800

    2019.03.01

--- webServer/app.js
+++ webServer/app.js
@@ -2,6 +2,10 @@ const http = require('http');
 const url  =  require('url');
 const fs  = require('fs');
 const path = require('path');
+// 引入 qs 模块：qs 是对路径进行 json 化或者将 json 转换为 string 路径
+const qs = require("querystring");
+const router  =  require('./app/router.js');
+
 
 const BAS_PATH = './public/';
 
@@ -9,14 +13,39 @@ http.createServer((request,response)=>{
 	let pathName = url.parse(request.url).pathname;
 	// 获取文件的后缀名
 	let extName =  path.extname(pathName);
-	
 
 	if(pathName ==  '/'){
 		pathName  = 'index.html';
 	} 
-
 	// 过滤 /favicon.ico 的请求
 	if(pathName != '/favicon.ico'){
+		let api = pathName.replace(/\//,'');
+		if(api in router.post){//POST接口
+			var reqData = '';
+			request.on('data',(chunk)=>{
+				reqData  += chunk;
+			})
+			request.on('end',()=>{
+				router.post[api](Object.assign({
+					parames:qs.parse(reqData)
+				},request),response);
+			});
+			return;
+		}
+
+		if(api in router.get){//GET接口
+			var reqData = '';
+			request.on('data',(chunk)=>{
+				reqData  += chunk;
+			})
+			request.on('end',()=>{
+				router.get[api](Object.assign({
+					parames:qs.parse(reqData)
+				},request),response);
+			});
+			return;
+		}
+		
 		fs.readFile( BAS_PATH + pathName,(error,data)=>{
 			if(error){
 				console.log('404  Not Found!');
@@ -29,11 +58,9 @@ http.createServer((request,response)=>{
 						});
 						response.write(_data);
 						response.end();	
-					}
-					
+					}						
 				});
 			}else{
-				console.log(`读取:${pathName}完成`);
 				let ext = getExt(extName);
 
 				response.writeHead(200,{
@@ -44,7 +71,10 @@ http.createServer((request,response)=>{
 			}
 		});
 	}
-}).listen(8080);
+	
+}).listen(8888);
+
+
 
 // 获取后缀名
 getExt = (extName) => {
---
+++ webServer/app/router.js
@@ -0,0 +1,102 @@
+const User =  require('./User.js');
+
+const router = {
+	get : {},
+	post: {},
+	add : function(method,route,fnc){
+		if(typeof fnc == 'function'){
+			this[method][route] =  fnc;
+			return  true;
+		}else{
+			console.error('typeof fnc is not fucntion!');
+		}
+	}
+};
+
+router.add('get','getMessage',(request,response)=>{
+	User().getMessage(request.parames).then((data)=>{
+		/*response.writeHead(200,{
+			"Content-Type":'text/html:chearset-UTF-8'
+		});*/
+		response.write(suress({
+			data:{
+				list:data
+			},
+			msg:'获取留言成功'
+		}),'utf-8');
+		response.end();
+	}).catch((error)=>{
+		new Error(error);
+	});
+});
+
+router.add('post','login',(request,response)=>{
+	User().postLogin(request.parames).then((data)=>{
+		/*response.writeHead(200,{
+			"Content-Type":'text/html;chearset-UTF-8'
+		});*/
+		console.log(data);
+		if(data.length){
+			response.write(suress({
+				data:{
+					id:data[0].id,
+					userName:data[0].user_name,
+				},
+				msg:'登录成功'
+			}),'utf-8');
+		}else{
+			response.write(suress({
+				data:{},
+				msg:'用户名或密码错误'
+			}),'utf-8');
+		}
+		
+		response.end();
+	}).catch((error)=>{
+		new Error(error);
+	});
+});
+
+router.add('post','register',(request,response)=>{
+	User().postRegister(request.parames).then((data)=>{
+		/*response.writeHead(200,{
+			"Content-Type":'text/html;chearset-UTF-8'
+		});*/
+
+		response.write(suress({
+			data:{
+				id: data.insertId,
+			},
+			msg:'注册成功'
+		}),'utf-8');
+		response.end();
+	}).catch((error)=>{
+		new Error(error);
+	});
+})
+
+router.add('post','sendMessage',(request,response)=>{
+	User().postSendMessage(request.parames).then((data)=>{
+		/*response.writeHead(200,{
+			"Content-Type":'text/html;chearset-UTF-8'
+		});*/
+		response.write(suress({
+			msg:'添加留言成功'
+		}),'utf-8');
+		response.end();
+	}).catch((error)=>{
+		new Error(error);
+	});
+})
+
+suress = (data)=>{
+
+	return JSON.stringify(
+		Object.assign({
+				code:200,
+				status:1,
+				data:{},
+				msg:''
+	},data));
+};
+module.exports  = router;
